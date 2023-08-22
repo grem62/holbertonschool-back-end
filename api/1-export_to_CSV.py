92d3b9d38bbffb68e7ae508729c9aea624430ba2
grem62 <gremontmatheo22@gmail.com>
2023-08-22 09:17:31 +0200

    add task 1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import requests
+from sys import argv
+import csv
+url_ap = "https://jsonplaceholder.typicode.com"
+
+
+def api():
+    user_response = requests.get(f"{url_ap}/users/{argv[1]}").json()
+    todo_response = requests.get(f"{url_ap}/todos?userId={argv[1]}").json()
+
+    with open(f"{argv[1]}.csv", 'w') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+
+        for task in todo_response:
+            writer.writerow([
+                user_response['id'],
+                user_response['username'],
+                task['completed'],
+                task['title']
+            ])
+
+
+if __name__ == "__main__":
+    api()
