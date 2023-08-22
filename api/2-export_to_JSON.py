51240c2a409c2bfe7275c93911408b0527c8f84e
grem62 <gremontmatheo22@gmail.com>
2023-08-22 10:37:32 +0200

    test code

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,32 +1,30 @@
-#!/usr/bin/python3
-"""Script to export data in the JSON format."""
 import json
 import requests
+from sys import argv
 
-API_URL = 'https://jsonplaceholder.typicode.com'
+
+def get_api():
+    """gather data from an API"""
+    url = 'https://jsonplaceholder.typicode.com/'
+    uid = argv[1]
+
+    # get a specific user from users in jsonplaceholder
+    usr = requests.get(url + 'users/{}'.format(uid)).json()
+    # make a query string to get tasks based on user id
+    todo = requests.get(url + 'todos', params={'userId': uid}).json()
+
+    with open('{}.json'.format(uid), 'w') as file:
+        obj = {uid: []}
+        for employee in todo:
+            tmp_obj = {
+                    'task': employee.get('title'),
+                    'completed': employee.get('completed'),
+                    'username': usr.get('username')
+                    }
+            obj[uid].append(tmp_obj)
+        # serialize object into a JSON string
+        json.dump(obj, file)
+
 
 if __name__ == '__main__':
-    # Récupérer les données de tous les utilisateurs depuis l'API
-    user_data = requests.get(f"{API_URL}/users").json()
-
-    # Créer un dictionnaire pour stocker les données de tous les employés
-    all_employee_data = {}
-
-    # Parcourir chaque user pour obtenir ses tâches et les stocker dans le dict
-    for user in user_data:
-        user_id = str(user['id'])
-        tasks_data = requests.get(f"{API_URL}/todos?userId={user_id}").json()
-
-        # Préparer les données des tâches pour l'utilisateur actuel
-        tasks_info = [
-            {"username": user['username'],
-                "task": task['title'],
-                "completed": task['completed']}
-            for task in tasks_data
-        ]
-
-        all_employee_data[user_id] = tasks_info
-
-    # Écrire les données dans un fichier JSON
-    with open("todo_all_employees.json", mode='w') as json_file:
-        json.dump(all_employee_data, json_file)
+    get_api()
