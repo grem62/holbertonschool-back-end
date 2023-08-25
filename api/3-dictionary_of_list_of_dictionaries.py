1ca5e13790f6fb4c8d1be63d8198e8874a0f5747
grem62 <gremontmatheo22@gmail.com>
2023-08-25 13:06:46 +0200

    correction task

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
-"""Script to export data in the JSON format."""
+"""
+description of script
+"""
 import json
 import requests
 
-API_URL = 'https://jsonplaceholder.typicode.com'
+if __name__ == "__main__":
 
+    URL = "https://jsonplaceholder.typicode.com"
 
-if __name__ == '__main__':
-    # Récupérer les informations des utilisateurs depuis l'API
-    user_response = requests.get(f"{API_URL}/users/").json()
-
-    # Récupérer la liste de tâches pour tous les utilisateurs
-    todo_response = requests.get(f"{API_URL}/todos").json()
-
-    # Créer un dictionnaire pour regrouper les tâches par utilisateur
-    task_by_user = {}
-    for task in todo_response:
-        user_id = task['userId']
-        if user_id not in task_by_user:
-            task_by_user[user_id] = []
-        task_by_user[user_id].append({
-            "task": task['title'],
-            "completed": task['completed'],
-            "username": next(
-            user['username'] for user in user_response if user['id'] == user_id)
-    })
-
-    # Écrire les données dans un fichier JSON
-    with open("todo_all_employees.json", mode='w') as json_file:
-        json.dump(task_by_user, json_file)
+    users = requests.get(f'{URL}/users').json()
+    dict_user = {}
+    for user in users:
+        tasks = requests.get(f'{URL}/users/{user["id"]}/todos').json()
+        dict_user[user['id']] = []
+        for task in tasks:
+            dict_task = {"task": task["title"], "completed": task["completed"],
+                         "username": user["username"]}
+            dict_user[user["id"]].append(dict_task)
+    with open("todo_all_employees.json", "w") as file:
+        json.dump(dict_user, file)
